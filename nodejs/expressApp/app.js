ae2ee10619e7419785c4fc8cea3c05e7b1c8f77f
kunljha <jha711kunal@gmail.com>
2021-11-18 23:08:50 +0530

    added route to addArticle

--- nodejs/expressApp/app.js
+++ nodejs/expressApp/app.js
@@ -9,7 +9,32 @@ app.set('view engine', 'pug')
 
 // home route
 app.get('/', (req, res) => {
-	res.send('Hello from Express App')
+	const articles = [
+		{
+			id: 1,
+			title: 'Article One',
+			body: 'This is aticle one body',
+			author: 'John Doe',
+		},
+		{
+			id: 2,
+			title: 'Article Two',
+			body: 'This is aticle two body',
+			author: 'Sam Smith',
+		},
+		{
+			id: 3,
+			title: 'Article Three',
+			body: 'This is aticle three body',
+			author: 'Ted Philips',
+		},
+	]
+	res.render('index', { title: 'Articles', articles: articles })
+})
+
+// add article route
+app.get('/articles/add', (req, res) => {
+	res.render('add_article', { title: 'Add a Article' })
 })
 
 const PORT = process.env.PORT || 5000
