46ec598476687fc7b86e1cc461e87922d0c99066
kunljha <jha711kunal@gmail.com>
2021-11-19 20:57:45 +0530

    saving articles to db

--- nodejs/expressApp/app.js
+++ nodejs/expressApp/app.js
@@ -8,21 +8,44 @@ const app = express() // initialiaze express app
 // connecting to database
 const PORT = process.env.PORT || 5000
 connectToDB()
-app.listen(PORT, () => {
-	console.log(`Server listening for requests on port: ${PORT}`)
-})
+	.then(() => {
+		app.listen(PORT, () =>
+			console.log(`Server listening for requests on port: ${PORT}`)
+		)
+	})
+	.catch((err) => console.log(err))
 
 // view engine setup
 app.set('views', path.resolve(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-// home route
+// middlewares
+app.use(express.urlencoded({ extended: true }))
+
+// get articles
 app.get('/', async (req, res) => {
 	try {
 		const articles = await Article.find()
 		res.render('index', { title: 'Articles', articles: articles })
 	} catch (err) {
 		console.log('cannot get articles!!')
+	}
+})
+
+// post new article
+app.post('/articles/add', async (req, res) => {
+	const { title, author, body } = req.body
+	try {
+		const article = new Article({
+			title,
+			author,
+			body,
+		})
+		await article.save()
+		res.redirect('/')
+	} catch (err) {
+		console.log('Cannot save article!!')
+		console.log(err)
 	}
 })
 
